2b8eda3a3d559e62ccf84dfacd6beedfdc2acb01
Jordan Kowal <kowaljordan@gmail.com>
2023-03-08 00:19:59 +0100

    linting

--- src/async/index.js
+++ src/async/index.js
@@ -1,3 +1,3 @@
-export { default as DebouncedFunction } from "./DebouncedFunction";
-export { default as sleep } from "./sleep";
-export { default as ThrottledFunction } from "./ThrottledFunction";
+export { default as DebouncedFunction } from './DebouncedFunction';
+export { default as sleep } from './sleep';
+export { default as ThrottledFunction } from './ThrottledFunction';
